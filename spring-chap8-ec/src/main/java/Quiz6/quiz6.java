596d933cd44ba0c770b9ff5f1c94b6b06a96077b
aaa <aaa@aaa.com>
2025-05-20 17:24:27 +0900

    0520ec

---
+++ spring-chap8-ec/src/main/java/Quiz6/quiz6.java
@@ -0,0 +1,14 @@
+package Quiz6;
+
+public class quiz6 {
+
+	public static void main(String[] args) {
+		for (int n = 1; n <= 10; n++) {
+			for (int i = 1; i <= 10; i++) {
+				System.out.print(i);
+			}
+			System.out.println("");
+		}
+
+	}
+}
